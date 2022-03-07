97b45d5c8a668d7e875a90db639c49f747c60eab
Jorge Osvaldo Varela <94763025+osvaldovarela@users.noreply.github.com>
2022-03-06 23:00:59 -0300

    inicio angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SwiperModule } from 'swiper/angular';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaYEducacionComponent } from './componentes/experiencia-y-educacion/experiencia-y-educacion.component';
+import { AptitudesComponent } from './componentes/aptitudes/aptitudes.component';
+import { LogrosComponent } from './componentes/logros/logros.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AcercaDeComponent,
+    ExperienciaYEducacionComponent,
+    AptitudesComponent,
+    LogrosComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule , SwiperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
+
+
